c1a65ad322359c470287a0750d6420f4b23effaa
rdfsx <m315498@gmail.com>
2021-11-13 21:13:10 +0300

    renamed get_my_status -> set_my_status

--- app/handlers/updates/status.py
+++ app/handlers/updates/status.py
@@ -5,10 +5,10 @@ from odmantic import AIOEngine
 from app.models import UserModel
 
 
-async def set_status(my_chat_member: ChatMemberUpdated, db: AIOEngine, user: UserModel):
+async def set_my_status(my_chat_member: ChatMemberUpdated, db: AIOEngine, user: UserModel):
     user.status = my_chat_member.new_chat_member.status
     await db.save(user)
 
 
 def setup(dp: Dispatcher):
-    dp.register_my_chat_member_handler(set_status)
+    dp.register_my_chat_member_handler(set_my_status)
