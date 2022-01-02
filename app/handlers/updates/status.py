3af8bf81b5718ac054cd674582c6f0bf941deaf2
kuttLoggin <123play123.loggin@gmail.com>
2022-01-02 19:50:23 +0700

    Fix status.py

--- app/handlers/updates/status.py
+++ app/handlers/updates/status.py
@@ -4,9 +4,11 @@ from aiogram.types import ChatMemberUpdated
 from app.models import UserModel
 
 
-async def set_my_status(my_chat_member: ChatMemberUpdated, user: UserModel):
-    user.status = my_chat_member.new_chat_member.status
-    await user.save()
+async def set_my_status(my_chat_member: ChatMemberUpdated):
+    user = await UserModel.find_one(UserModel.id == my_chat_member.from_user.id)
+    if user:
+        user.status = my_chat_member.new_chat_member.status
+        await user.save()
 
 
 def setup(dp: Dispatcher):
