f27eafbc4f9d8b5b67953a97497f6533dfb6a5b9
rdfsx <m315498@gmail.com>
2022-01-02 16:31:33 +0300

    :construction: removed dumbass i18n

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,12 +1,9 @@
-from pathlib import Path
-
 from aiogram import Dispatcher
 from aiogram.contrib.middlewares.environment import EnvironmentMiddleware
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 from .acl import ACLMiddleware
 from .clocks import ClocksMiddleware
-from .i18n import I18nMiddleware
 from .throttling import ThrottlingMiddleware
 
 
@@ -16,7 +13,3 @@ def setup(dp: Dispatcher):
     dp.setup_middleware(ACLMiddleware())
     dp.setup_middleware(ThrottlingMiddleware())
     dp.setup_middleware(ClocksMiddleware())
-
-
-def setup_i18n(dp: Dispatcher, path: Path, default: str = "en"):
-    dp.setup_middleware(I18nMiddleware(path, default))
